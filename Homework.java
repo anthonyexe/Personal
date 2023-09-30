658b66de8629a0af3ad8c1bc2c4d945c6cb5160f
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-09-30 12:03:11 -0400

    Add files via upload

--- Homework.java
+++ Homework.java
@@ -2,7 +2,8 @@ import java.util.*;
 import java.text.DecimalFormat;
 
 public class Homework {
-	public HashMap<Character, Double> relativeFrequencies(String plaintext) {
+	
+	public HashMap<Character, Double> frequencyAnalysis(String plaintext) {
 		HashMap<Character, Integer> frequencies = new HashMap<Character, Integer>();
 		HashMap<Character, Double> rFrequencies = new HashMap<Character, Double>(frequencies.size());
 		DecimalFormat df = new DecimalFormat("#.000");
@@ -33,11 +34,12 @@ public class Homework {
 	}
 	
 	public String shiftCipherEncryption(String plaintext, int key) {
-		final int alphabetSize = 26;
-		HashMap<Integer, Character> alphabet = new HashMap<Integer, Character>(alphabetSize);
 		String ciphertext = "";
 		
-		if (key > 25 || key < 0) {
+		final int alphabetSize = 26;
+		HashMap<Integer, Character> alphabet = new HashMap<Integer, Character>(alphabetSize);
+		
+		if (key > 25 || key < 1) {
 			return "Invalid Key";
 		}
 		
@@ -71,6 +73,35 @@ public class Homework {
 	public String shiftCipherDecryption(String ciphertext) {
 		String plaintext = "";
 		
+		final int alphabetSize = 26;
+		HashMap<Integer, Character> alphabet = new HashMap<Integer, Character>(alphabetSize);
+		
+		Character ch = 'a';
+		for (Integer i = 0; i < alphabetSize; i++) {
+			if (ch == 'a') {
+				alphabet.put(i, ch);
+				ch++;
+			}
+			else {
+				alphabet.put(i, ch);
+				ch++;
+			}
+			
+		}
+		
+		for (int i = 0; i < ciphertext.length(); i++) {
+			Character current = Character.toLowerCase(ciphertext.charAt(i));
+			
+			if (!Character.isWhitespace(current)) {
+				Integer currentIndex = current - 'a' + 1;
+				Integer shiftedIndex = (currentIndex - 16) % 26;
+				if (shiftedIndex < 0)
+					shiftedIndex += 26;
+				
+				Character shiftedChar = alphabet.get(shiftedIndex);
+				plaintext += shiftedChar;
+			}
+		}
 		
 		return plaintext;
 	}
--- HomeworkTest.java
+++ HomeworkTest.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class HomeworkTest {
 	public static void main(String[] args) {
 		Homework hw = new Homework();
-		/*
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Please enter some text: ");
 		String input = scan.nextLine();
 		
+		//System.out.println("Please enter a key value: ");
+		//int key = scan.nextInt();
 		
-		System.out.println(hw.relativeFrequencies(input));
-		*/
 		
-		System.out.println(hw.shiftCipherEncryption("Hello World", 1));
+		System.out.println(hw.frequencyAnalysis(input));
+		System.out.println(hw.shiftCipherDecryption(input));
 	}
 }
