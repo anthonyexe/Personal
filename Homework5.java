bbcd864981d6659c902cf0c99876c0ebb26c93f9
anthonyexe <47644214+anthonyexe@users.noreply.github.com>
2023-11-26 22:02:21 -0500

    Add files via upload

--- Homework5.java
+++ Homework5.java
@@ -1,15 +1,51 @@
 //Anthony D'Alessandro
 public class Homework5 {
-	public int[] polynomialAddition(int[] poly1, int[] poly2, int m) {
-		int[] finalPolynomial = new int[m];
+	public int[] polynomialAddition(int[] poly1, int[] poly2) {
+		if (poly1.length > poly2.length) {
+			int[] newPoly2 = new int[poly1.length];
+			int difference = poly1.length - poly2.length;
+			for (int i = 0; i < newPoly2.length; i++) {
+				if (i < difference) {
+					newPoly2[i] = 0;
+				}
+				else {
+					newPoly2[i] = poly2[i - difference];
+				}
+			}
+			int[] finalPolynomial = new int[poly1.length];
+			for (int i = 0; i < finalPolynomial.length; i++) {
+				finalPolynomial[i] = ((poly1[i] + newPoly2[i]) % 2);
+			}
+			return finalPolynomial;
+		}
 		
-		for (int i = 0; i < m; i++)
-			finalPolynomial[i] = (poly1[i] + poly2[i]) % 2;
-		
-		return finalPolynomial;
+		else if (poly2.length > poly1.length) {
+			int[] newPoly1 = new int[poly2.length];
+			int difference = poly2.length - poly1.length;
+			for (int i = 0; i < newPoly1.length; i++) {
+				if (i < difference) {
+					newPoly1[i] = 0;
+				}
+				else {
+					newPoly1[i] = poly2[i - difference];
+				}
+			}
+			int[] finalPolynomial = new int[poly2.length];
+			for (int i = 0; i < finalPolynomial.length; i++) {
+				finalPolynomial[i] = ((poly1[i] + newPoly1[i]) % 2);
+			}
+			return finalPolynomial;
+		}
+		else {
+			int[] finalPolynomial = new int[poly1.length];
+			for (int i = 0; i < finalPolynomial.length; i++)
+				finalPolynomial[i] = (poly1[i] + poly2[i]) % 2;
+			
+			return finalPolynomial;
+		}
 	}
 	
-	public int[] polynomialMultiplication(int[] poly1, int[] poly2, int m) {
+	public int[] standardPolynomialMultiplication(int[] poly1, int[] poly2, int m) {
 		final int finalMaxLength = (m * 2) - 1;
 		int[] finalPolynomial = new int[finalMaxLength];
 		
@@ -30,21 +66,37 @@ public class Homework5 {
 		return finalPolynomial;
 	}
 	
-	public int[] reducedPolynomialMultiplication(int[] product, int[] irrPoly, int m) {
-		int productHighestDegree = product.length - 1;
-		int irrPolyLength = irrPoly.length;
+	public int[] GF2MPolynomialMultiplication(int[] product, int[] irrPoly, int m) {
+		int[] reducedProduct;
+		int productHighestDegree = 0;
+		for (int i = 0; i < product.length; i++) {
+			if (product[i] == 1) {
+				productHighestDegree = product.length - (i + 1);
+				break;
+			}
+		}
 		
-		if (productHighestDegree >= m) {
-			int degreeDifference = product.length - irrPolyLength;
+		if (productHighestDegree < m)
+			return product;
+		
+		int irrPolyLength = irrPoly.length;
+		int degreeDifference = productHighestDegree - (irrPolyLength - 1);
+		if (degreeDifference > 0) {
 			int[] tempArray = new int[irrPolyLength + degreeDifference];
 			for (int i = 0; i < irrPolyLength; i++) {
 				if (irrPoly[i] == 1) {
-					
-					tempArray[]	
+					tempArray[i] = 1;	
 				}
 			}
+			reducedProduct = polynomialAddition(product, tempArray);
+			if (productHighestDegree >= m - 1)
+				reducedProduct = GF2MPolynomialMultiplication(reducedProduct, irrPoly, m);
 		}
-		else
-			return product;
+		else {
+			reducedProduct = polynomialAddition(product, irrPoly);
+			return reducedProduct;
+		}
+		
+		return reducedProduct;
 	}
 }
--- Tester.java
+++ Tester.java
@@ -6,8 +6,6 @@ public class Tester {
 		Scanner scan = new Scanner(System.in);
 		Homework5 hw5 = new Homework5();
 		
-		
-		
 		System.out.println("Please enter the highest polynomial degree: ");
 		int m = scan.nextInt() + 1;
 		
@@ -35,62 +33,86 @@ public class Tester {
 			irrPoly[i] = scan.nextInt();
 		}
 		
-		System.out.print("Polynomial 1 + Polynomial 2 =  ");
-		int[] sum = hw5.polynomialAddition(poly1, poly2, m);
-		for (int i = 0; i < m; i++) {
+		System.out.print("Polynomial 1 + Polynomial 2 (GF2^M) =  ");
+		int[] sum = hw5.polynomialAddition(poly1, poly2);
+		ArrayList<String> printedSum = new ArrayList<String>();
+		for (int i = 0; i < sum.length; i++) {
 			if (sum[i] != 0) {
-				if (i < m - 1)
-					System.out.print("x^" + (m - (i + 1)) + " + ");
-				else
-					System.out.print(sum[i] +"\n");
+				String value;
+				if (i == sum.length - 1) {
+					value = "1";
+				}
+				else if (i == sum.length - 2) {
+					value = "x";
+				}
+				else {
+					value = "x^" + (sum.length - (i + 1));
+				}
+				printedSum.add(value);
 			}
+		}
+		
+		for (int i = 0; i < printedSum.size(); i++) {
+			if (i < printedSum.size() - 1)
+				System.out.print(printedSum.get(i) + " + ");
+			else
+				System.out.print(printedSum.get(i));
 		}
 		
 		System.out.println();
 		
 		System.out.print("Polynomial 1 * Polynomial 2 = ");
-		int[] product = hw5.polynomialMultiplication(poly1, poly2, m);
-		int productLength = product.length;
-		for (int i = 0; i < productLength; i++) {
+		int[] product = hw5.standardPolynomialMultiplication(poly1, poly2, m);
+		ArrayList<String> printedProduct = new ArrayList<String>();
+		for (int i = 0; i < product.length; i++) {
 			if (product[i] != 0) {
-				if (i < productLength - 1)
-					System.out.print("x^" + (productLength - (i + 1)) + " + ");
-				else
-					System.out.print(product[i]);
+				String value;
+				if (i == product.length - 1) {
+					value = "1";
+				}
+				else if (i == product.length - 2) {
+					value = "x";
+				}
+				else {
+					value = "x^" + (product.length - (i + 1));
+				}
+				printedProduct.add(value);
 			}
 		}
 		
-		/*
-		 * 
-		//Solve Problem 1
-		System.out.println("-------- Problem 1 --------\n");
-		System.out.println("Enter plaintext for Playfair Encryption: ");
-		String plaintext = scan.nextLine();
+		for (int i = 0; i < printedProduct.size(); i++) {
+			if (i < printedProduct.size() - 1)
+				System.out.print(printedProduct.get(i) + " + ");
+			else
+				System.out.print(printedProduct.get(i));
+		}
 		
-		System.out.println("Enter the encryption key: ");
-		String key = scan.nextLine();
+		System.out.println();
 		
-		System.out.println("Ciphertext: " + ex.solveNumber1(plaintext, key));
-		System.out.println("\n-------- End of Problem 1 --------\n");
-		
-		
-		//Solve Problem 3
-		System.out.println("-------- Problem 3 --------\n");
-		System.out.println("Enter affine ciphertext: ");
-		String ciphertext = scan.nextLine();
-		
-		System.out.println("Plaintext & Key: " + ex.solveNumber3(ciphertext));
-		System.out.println("\n-------- End of Problem 3 --------\n");
-		
-		
-		//Solve Problem 4
-		System.out.println("-------- Problem 4 --------\n");
-		System.out.println("Enter text for Index of Coincidence Test: ");
-		String text = scan.nextLine();
-		
-		System.out.println("Index of Coincidence: " + ex.solveNumber4(text));
-		System.out.println("\n-------- End of Problem 4 --------");
-		
-		*/
+		System.out.print("Polynomial 1 * Polynomial 2 (GF2^M) = ");
+		int[] finalProduct = hw5.GF2MPolynomialMultiplication(product, irrPoly, m);
+		ArrayList<String> printedProduct2 = new ArrayList<String>();
+		for (int i = 0; i < finalProduct.length; i++) {
+			if (finalProduct[i] != 0) {
+				String value;
+				if (i == finalProduct.length - 1) {
+					value = "1";
+				}
+				else if (i == finalProduct.length - 2) {
+					value = "x";
+				}
+				else {
+					value = "x^" + (finalProduct.length - (i + 1));
+				}
+				printedProduct2.add(value);
+			}
+		}
+			
+		for (int i = 0; i < printedProduct2.size(); i++) {
+			if (i < printedProduct2.size() - 1)
+				System.out.print(printedProduct2.get(i) + " + ");
+			else
+				System.out.print(printedProduct2.get(i));
+		}
 	}
 }
